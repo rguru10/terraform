17585a79604c51375e4c3cf58ee458d57f634b1e
Ratnasri Guruju <rguru10@pcw1937747l.safeway01.ad.safeway.com>
2019-04-30 14:08:53 -0700

    Adding terraform

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ resource "random_id" "project_name" {
   byte_length = 4
 }
 
+# generate sql password
+resource "random_id" "sql_password" {
+  byte_length = 32
+}
+
 # Local for tag to attach to all items
 locals {
   tags = "${merge(var.tags, map("ProjectName", random_id.project_name.hex))}"
@@ -69,4 +74,98 @@ data "template_file" "setup" {
   vars {
     vault_url             = "${var.vault_url}"
   }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                          = "${random_id.project_name.hex}-vm"
+  location                      = "${azurerm_resource_group.main.location}"
+  resource_group_name           = "${azurerm_resource_group.main.name}"
+  network_interface_ids         = ["${azurerm_network_interface.main.id}"]
+  vm_size                       = "Standard_A2_v2"
+  delete_os_disk_on_termination = true
+
+  identity = {
+    type = "SystemAssigned"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${random_id.project_name.hex}vm-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${random_id.project_name.hex}vm"
+    admin_username = "ubuntu"
+    admin_password = "Password1234!"
+    custom_data    = "${data.template_file.setup.rendered}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "virtual_machine_extension" {
+  name                 = "vault"
+  location             = "${var.location}"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_machine_name = "${azurerm_virtual_machine.main.name}"
+  publisher            = "Microsoft.ManagedIdentity"
+  type                 = "ManagedIdentityExtensionForLinux"
+  type_handler_version = "1.0"
+
+  settings = <<SETTINGS
+    {
+        "port": 50342
+    }
+SETTINGS
+}
+
+resource "azurerm_mysql_server" "sql" {
+  name                         = "${random_id.project_name.hex}-mysql"
+  resource_group_name          = "${azurerm_resource_group.main.name}"
+  location                     = "${azurerm_resource_group.main.location}"
+
+  sku {
+    name     = "B_Gen5_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen5"
+  }
+
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+
+  administrator_login          = "sqladmin"
+  administrator_login_password = "${random_id.sql_password.id}"
+  version                      = "5.7"
+  ssl_enforcement              = "Disabled"
+}
+
+resource "azurerm_mysql_database" "database" {
+  name                = "exampledb"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  server_name         = "${azurerm_mysql_server.sql.name}"
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+}
+
+resource "azurerm_mysql_firewall_rule" "sql" {
+  name                = "FirewallRule1"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  server_name         = "${azurerm_mysql_server.sql.name}"
+  start_ip_address    = "${azurerm_public_ip.main.ip_address}"
+  end_ip_address      = "${azurerm_public_ip.main.ip_address}"
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,27 @@ output "download vault" {
   value       = "sudo wget ${var.vault_url} -P /usr/local/bin/vault; sudo chmod 755 /usr/local/bin/vault"
   description = "Command to download and install Vault binary"
 }
+
+output "PostgreSQL Server FQDN" {
+  value = "${azurerm_mysql_server.sql.fqdn}"
+}
+
+output "PostgreSQL Server Username/Password" {
+  value = "${azurerm_mysql_server.sql.administrator_login}\\${azurerm_mysql_server.sql.administrator_login_password}"
+}
+
+output "Subscription ID" {
+  value = "${data.azurerm_client_config.current.subscription_id}"
+}
+
+output "Resource Group Name" {
+  value = "${azurerm_virtual_machine.main.resource_group_name}"
+}
+
+output "VM Name" {
+  value = "${azurerm_virtual_machine.main.name}"
+}
+
+output "JWT Token" {
+  value = "jwt=`curl http://localhost:50342/oauth2/token?resource=https://management.azure.com -H metadata:true | jq .access_token | tr -d '\"'"
+}
